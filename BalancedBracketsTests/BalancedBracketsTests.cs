761cd4ee1b9dae8f365683064d6273a84f789c51
Meghan <stlmeghan@yahoo.com>
2022-08-31 21:56:43 -0500

    done

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -13,5 +13,92 @@ namespace BalancedBracketsTests
         {
             Assert.AreEqual(true, true);
         }
+
+        //Test 1:
+        [TestMethod]
+        public void OnlyBracketsReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]"));
+        }
+
+        //Test 2:
+        [TestMethod]
+        public void BracketsWithStringInsideReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[LaunchCode]"));
+        }
+
+        //Test 3:
+        [TestMethod]
+        public void BracketsWithinStringReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("Launch[Code]"));
+        }
+
+        //Test 4:
+        [TestMethod]
+        public void BracketsAtBeginningReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]LaunchCode"));
+        }
+
+        //Test 5:
+        [TestMethod]
+        public void EmptyStringReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets(""));
+        }
+
+        //Test 6:
+        [TestMethod]
+        public void BracketPairsReturnTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[][]"));
+        }
+
+        //Test 7:
+        [TestMethod]
+        public void NestedBracketsReturnTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[[]]"));
+        }
+
+        //Test 8: Failed
+        [TestMethod]
+        public void OnlyReversedBracketsReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("]["));
+        }
+
+        //Test 9:
+        [TestMethod]
+        public void SingleBracketAtFrontReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("[LaunchCode"));
+        }
+
+        //Test 10: Failed
+        [TestMethod]
+        public void BracketsReversedInStringReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("La]nchC[ode"));
+        }
+
+        //Test 11:
+        [TestMethod]
+        public void SingleBracketReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("["));
+        }
+
+        //Test 12: Failed
+        [TestMethod]
+        public void MixedBracketsReturnFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("][[]"));
+        }
+
+
+
     }
 }
